12680656c19cf0f75914f647ca68c2e02a81e5d5
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-06-14 02:48:46 +0500

    created a book object

--- javascript/script.js
+++ javascript/script.js
@@ -8,9 +8,27 @@ const playerN = {
 // using function to create an object
 
 function player(name,marker) {
+    if (!new.target){
+        throw Error("you must use the 'new' constructor when calling this function")
+    }
     this.name = name
     this.marker = marker
 }
 
 const player1 = new player("ahmad","O")
-console.log(player1.name)
+const player2 = new player("bilal","X")
+// console.log(player1.name)
+// creating a book object
+
+const book = {
+    nameBook : "The Hobbit",
+    author : "J.R.R Tolkein",
+    pages : 297,
+    read: false,
+    info  : function(){
+        const isRead = (val) => val == true ? 'read' : 'not read yet';
+        return `${book.nameBook} by ${book.author},${book.pages} pages, ${isRead(book.read)} `;
+    }
+}
+
+console.log(book.info())
