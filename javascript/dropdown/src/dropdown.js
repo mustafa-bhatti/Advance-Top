e14960b2a22b20b07d5995fc5fb7010e0686c139
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-07-21 00:00:47 +0500

    importing the package as nodeJs module

--- javascript/dropdown/src/dropdown.js
+++ javascript/dropdown/src/dropdown.js
@@ -1,8 +1,7 @@
-const dropdownHover = function (buttonClass, dropDownClass) {
+export const dropdownHover = function (buttonClass, dropDownClass) {
   const button = document.querySelector(`.${buttonClass}`);
   const dropdownDiv = document.querySelector(`.${dropDownClass}`);
   button.addEventListener('mouseover', () => {
     dropdownDiv.classList.toggle('hide');
   });
-}
-module.exports = dropdownHover
+};
--- javascript/dropdown/test/script.js
+++ javascript/dropdown/test/script.js
@@ -1,2 +1,3 @@
-import dropdownHover from 'dropdown';
-console.log(dropdownHover)       
+import { dropdownHover } from "dropdown-tor-test";   
+const sd = require('dropdown-tor-test/')  
+console.log(sd.dropdownHover)
