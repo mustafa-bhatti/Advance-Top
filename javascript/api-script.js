968971ffe4022acdee8d0c01f84506646892cfa1
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-07-25 20:27:14 +0500

    search and refresh button work without error

--- javascript/api-script.js
+++ javascript/api-script.js
@@ -1,20 +1,32 @@
 const img = document.querySelector('img');
 const refreshBtn = document.querySelector('button');
-function fetchGif() {
-  fetch(
-    'https://api.giphy.com/v1/gifs/translate?api_key=aWFQln3fRt0IuEDfY6MvZ6q7dazqJw9s&s=karting',
+const form = document.querySelector("form")
+const query = document.querySelector("#search")
+async function fetchGif(query) {
+  await fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=aWFQln3fRt0IuEDfY6MvZ6q7dazqJw9s&s=${query}`,
     { mode: 'cors' }
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      console.log('refresh');
+      console.log(response);
       // console.log(response.data.images);
       img.src = response.data.images.original.url;
+
+    }).catch(function(response){
+        alert(response)
+        console.log("ERROR: ",response)
     });
 }
-fetchGif();
+fetchGif("Football");
 refreshBtn.addEventListener('click', () => {
-  fetchGif();
+  fetchGif("Football");
 });
+form.addEventListener("submit",function(event){
+    event.preventDefault()
+    let searchQuery = query.value
+    console.log(searchQuery)
+    fetchGif(searchQuery)
+})
