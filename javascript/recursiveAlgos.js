0a6b1893b5a3204d8d690e60a889d0d26ebf6013
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-07-29 08:28:01 +0500

    add merge sort algo

--- javascript/recursiveAlgos.js
+++ javascript/recursiveAlgos.js
@@ -17,5 +17,38 @@ function recurFib(num) {
   }
   return recurFib(num - 2) + recurFib(num - 1);
 }
-const fib = recurFib(7);
-console.log(fib);
+// const fib = recurFib(7);
+// console.log(fib);
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  const result = [];
+  let l = 0,
+    r = 0;
+  while (l < left.length && r < right.length) {
+    if (left[l] < right[r]) {
+      result.push(left[l]);
+      l++;
+    } else {
+      result.push(right[r]);
+      r++;
+    }
+  }
+  // Add remaining elements
+  return result.concat(left.slice(l)).concat(right.slice(r));
+}
+
+// Example usage:
+const unsorted = [3, 2, 1, 13, 8, 5, 0, 1];
+const sorted = mergeSort(unsorted);
+console.log(sorted); // [0, 1, 1, 2, 3,
